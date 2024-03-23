9462c419b510bd151ed9f6a5ba5c95604353baa3
yokomotod <yokomotod@gmail.com>
2024-03-23 20:25:56 +0900

    compare with the oldest new commit

--- cruft/_commands/utils/cruft.py
+++ cruft/_commands/utils/cruft.py
@@ -1,11 +1,12 @@
 import json
+from datetime import date, datetime
 from pathlib import Path
 from typing import Any, Dict, Optional
 
 from git import Repo
+from git.objects import Commit
 
 from cruft.exceptions import CruftAlreadyPresent, NoCruftFound
-from datetime import datetime, date
 
 CruftState = Dict[str, Any]
 
@@ -26,28 +27,51 @@ def get_cruft_file(project_dir_path: Path, exists: bool = True) -> Path:
 
 def is_project_updated(
     repo: Repo,
-    current_commit: str,
-    latest_commit: str,
+    current_commit_hash: str,
+    latest_commit_hash: str,
     strict: bool,
     allowed_delay_days: Optional[int] = None,
 ) -> bool:
-    latest_commit_date = datetime.fromtimestamp(repo.commit(latest_commit).committed_date).date()
-
-    return (
+    if (
         # If the latest commit exactly matches the current commit
-        latest_commit == current_commit
+        latest_commit_hash == current_commit_hash
         # Or if there have been no changes to the cookiecutter
-        or not repo.index.diff(current_commit)
-        # or if the strict flag is off, we allow for newer commits to count as up to date
-        or (
-            repo.is_ancestor(repo.commit(latest_commit), repo.commit(current_commit)) and not strict
-        )
-        or (
-            allowed_delay_days is not None
-            and repo.is_ancestor(repo.commit(current_commit), repo.commit(latest_commit))
-            and (date.today() - latest_commit_date).days <= allowed_delay_days
-        )
+        or not repo.index.diff(current_commit_hash)
+    ):
+        return True
+
+    latest_commit = repo.commit(latest_commit_hash)
+    current_commit = repo.commit(current_commit_hash)
+
+    # Or if the strict flag is off, we allow newer commits to count as up to date
+    if not strict and repo.is_ancestor(latest_commit, current_commit):
+        return True
+
+    # Or if allowed_delay_days is specified, we allow for not being the latest within that period
+    if allowed_delay_days is not None and _is_within_allowed_delay_days(
+        repo, current_commit, latest_commit, allowed_delay_days
+    ):
+        return True
+
+    return False
+
+
+def _is_within_allowed_delay_days(
+    repo: Repo, current_commit: Commit, latest_commit: Commit, allowed_delay_days: int
+) -> bool:
+    # Compare to the oldest, not imported commit's date in template repo. Why not use ...
+    # * latest_commit?  -- If the template repo continues to be updated continuously, we will be allowed to be behind indefinitely.
+    # * current_commit? -- If the last commit of the template repo is old, we will be judged as older than allowed_delay_days immediately when next update comes.
+    oldest_update_commit = next(
+        repo.iter_commits(f"{current_commit}..{latest_commit}", reverse=True),
+        None,
     )
+    if not oldest_update_commit:
+        return False
+
+    oldest_update_date = datetime.fromtimestamp(oldest_update_commit.committed_date).date()
+
+    return (date.today() - oldest_update_date).days <= allowed_delay_days
 
 
 def json_dumps(cruft_state: Dict[str, Any]) -> str:
